2db01766793d7cbb7b0055be72b87d1f4968e619
Max DeSousa <maxdesousa@protonmail.com>
2022-07-08 02:23:10 -0400

    While working at the bar, this sample broke
    I'm trying to import a simple out-of-tree library.
    But, the #include statement is failing. I'm commiting
    this content on a branch so I can maybe evaluate
    with better tools on my desktop

--- 01-Code/00-pico/includes/ZEDF9P/ZED-F9P.c
+++
@@ -1,6 +0,0 @@
-#include "ZED-F9P.h"
-#include "hardware/i2c.h"
-
-uint8_t InitModule(void) {
-
-}
--- 01-Code/00-pico/picoLibs/custom_pico.h
+++
@@ -1,8 +0,0 @@
-#ifndef CUSTOM_PICO_FUNCTIONS
-#define CUSTOM_PICO_FUNCTIONS
-
-uint8_t pico_InitI2cBus(uint8_t _sclPin, uint8_t _sdaPin, uint8_t _baud);
-uint8_t pico_InitSpiBus();
-uint8_t pico_InitUardBus();
-
-#endif
