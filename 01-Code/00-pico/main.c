66f7d2bcb9375ea327e94801e52b2227fadba1b9
Max DeSousa <maxdesousa@protonmail.com>
2022-07-10 17:44:18 -0400

    Making some small changes to make compatible
    with micro tools update. I've added the precompiler
    library to micro tools. I tested here quicly to make
    sure that it works.
    
    Outside of that - I made sure my i2c function was compiling...
    being very precautions after last CMake error took so long to
    figure out.

--- 01-Code/00-pico/main.c
+++ 01-Code/00-pico/main.c
@@ -1,9 +1,10 @@
+#include "microcontroller/pico.h"
 #include "simplePico/i2c.h"
 
 int main(void) 
   {
   uint8_t error = 0;
 
-  SetupI2cBus(i2c0, 1, 2, 115200); 
+  SetupI2cBus(1, 2, 115200); 
   return error;
   }
