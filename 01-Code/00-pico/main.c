7c3b35224280160cfe171094053c07f41fec9b2c
Max DeSousa <maxdesousa@protonmail.com>
2022-07-13 00:58:08 -0400

    Adding the new ZedF9p setup structure

--- 01-Code/00-pico/main.c
+++ 01-Code/00-pico/main.c
@@ -1,10 +1,16 @@
 #include "microcontroller/pico.h"
 #include "simplePico/i2c.h"
+#include "gps/zedf9p.h"
 
 int main(void) 
   {
   uint8_t error = 0;
 
-  SetupI2cBus(1, 2, 115200); 
+  //SetupI2cBus(1, 2, 115200); 
+  i2c_struct i2c_setup;
+  i2c_setup.dataPin = 1;
+  i2c_setup.clockPin = 2;
+  i2c_setup.baudRate = 115200;
+  //SimpleSetupZedf9p(i2c, &i2c_setup);
   return error;
   }
