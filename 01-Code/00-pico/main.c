e0166f259295dd1aef4c85f86bdcb5b6d6e943fe
Max DeSousa <maxdesousa@protonmail.com>
2022-07-21 23:41:58 -0400

    Making sure my changes to microlib is compiling

--- 01-Code/00-pico/main.c
+++ 01-Code/00-pico/main.c
@@ -4,7 +4,36 @@
 
 int main(void) 
   {
-  uint8_t error = 0;
+  /* Creating local variables */
+  uint8_t error;
+  bool startSurveyMode;
+  uint32_t surveyTime;
+  uint32_t surveyAccuracy;
+  T_ZF9P_s_ModuleConfig zedf9pSurveyBaseModule;
+  T_ZF9P_s_ModuleConfig* zedf9pSurveyBasePointer;
+
+  /* Initialize local variables */
+  error = 0;
+  surveyTime = 0;
+  surveyAccuracy = 0;
+  startSurveyMode = false;
+  zedf9pSurveyBasePointer = &zedf9pSurveyBaseModule;
+
+  /* Configuring ZEDF9P object */
+  InitializeZedf9pObject(zedf9pSurveyBasePointer);
+  zedf9pSurveyBaseModule.commsMethod = i2c;
+  zedf9pSurveyBaseModule.sdaPin = 0;
+  zedf9pSurveyBaseModule.clockPin = 1;
+  zedf9pSurveyBaseModule.baudRate = 115200;
+
+  /* First step - setup interrupts for rotary encoder for survey modification */
+
+  while(startSurveyMode == false)
+    {
+    ;/* do nothing while waiting for user to start survey mode */
+    }
+
+  SimpleSetupSurveyInBase(zedf9pSurveyBasePointer, surveyTime, surveyAccuracy);
 
   //SetupI2cBus(1, 2, 115200); 
   i2c_struct i2c_setup;
